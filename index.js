bd3270534b08acbf1ba35d9c91c6cd83895b3601
Matr3x <charlysa45hotmail.com>
2021-10-29 10:53:49 -0500

    socket

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server, {
   cors:{
-    origin: "http://localhost:3000",    
+    origin: "https://origaming-frontend.herokuapp.com/",    
     methods: ["GET", "POST"],
     allowedHeaders: ["chat-header"],    
     credentials: true
